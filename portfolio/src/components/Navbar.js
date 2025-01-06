880df7af7076c15d95fb0ba1a793c4c5eb0d46fa
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2025-01-06 16:51:18 +0100

    Worked on and implemented a simple navbar, styling not final just temporary

---
+++ portfolio/src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  // State for opening/closing of hamburgermenu 
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="logo">
+    
+      </div>
+      <ul className={`nav-links ${menuOpen ? 'active' : ''}`}>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
